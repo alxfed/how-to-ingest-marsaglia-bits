73451e0eb9f9796eefd932316a9739fa1d937ae8
alxfed <alxfed@gmail.com>
2018-04-28 15:56:10 -0500

    added dimension change in R

--- read_config.R
+++ read_config.R
@@ -5,14 +5,14 @@ require(ConfigParser)
 # Constant - the file name
 CONFIG_FILE_NAME <- 'data_config.ini'    
 
-# Creat a an instance of the object of ConfigParser class. 
+# Create an instance of the object of ConfigParser class. 
 
 config <- ConfigParser$new(init = NULL, optionxform = tolower)  
 
 # Read the file, its content will populate the object
 config$read(CONFIG_FILE_NAME, 
             encoding = getOption("encoding")
-            )
+            )      # of course if the "encoding" is there.
 
 # Now everything is accessible (as strings)
 s1 <- config$get(option = "x", 
